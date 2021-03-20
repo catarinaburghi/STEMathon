7503fdaefb7559ec78faafab88e5db3a25ccfc41
Catarina <cf.burghi@gmail.com>
2021-03-20 15:49:12 -0400

    Probability edits

--- Probability.java
+++ Probability.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 
 public class Probability {
@@ -27,33 +26,4 @@ public class Probability {
         System.out.print(" drowned.");
     }
 }
-=======
-import java.util.Scanner;
 
-public class Probability {
-
-    // variables that don't change
-    private static final double PERC = 0.91;
-
-    public static void main(String[] args) {
-
-        // Declaring my variables
-        Scanner sc = new Scanner(System.in);
-        int kids;
-        
-        System.out.println("According to the National Autism Association in June of 2016,");
-        System.out.println("The risk of death by drowning is twice as high for children with ASD than in the General population");
-        System.out.println("In 2011, accidental drowning accounted for 91% of injury-related deaths in the U.S among kids with ASD");
-        System.out.println("ages 14 years and younger.");
-        System.out.println("Please enter the number of children with ASD: ");
-        kids = sc.nextInt();
-
-        double drowned = kids * PERC;
-        
-
-        System.out.print("Out of " + kids);
-        System.out.print(" kids with disabilities, " + drowned);
-        System.out.print(" drowned.");
-    }
-}
->>>>>>> main
